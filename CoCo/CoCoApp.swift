097bd6fb615115a2f31e0ab0b6b7bca42b806f2d
Henry <c.henry.9209@gmail.com>
2025-06-30 14:28:05 +0900

    Initial Commit

---
+++ CoCo/CoCoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoCoApp.swift
+//  CoCo
+//
+//  Created by Henry on 6/30/25.
+//
+
+import SwiftUI
+
+@main
+struct CoCoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoCoTests/CoCoTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoCoTests.swift
+//  CoCoTests
+//
+//  Created by Henry on 6/30/25.
+//
+
+import Testing
+@testable import CoCo
+
+struct CoCoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
