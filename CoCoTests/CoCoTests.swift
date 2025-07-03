702e9bdb14d8069c12a0618b60f1cbd2c5c6762a
Henry <c.henry.9209@gmail.com>
2025-07-03 16:12:02 +0900

    02:Add TDD-Red flag Test

--- CoCoTests/CoCoTests.swift
+++ CoCoTests/CoCoTests.swift
@@ -5,13 +5,43 @@
 //  Created by Henry on 6/30/25.
 //
 
-import Testing
 @testable import CoCo
+import Testing
 
-struct CoCoTests {
+@Suite("FetchNearbyFeeds - 주변 피드 검색 테스트")
+struct FetchNearbyFeedsTests {
+    // MARK: - Factory
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    private func makeSUT() -> (
+        //        sut: /**/, mock: /**/
+    ) {}
+
+    // 초기 1. RED FLAG TEST
+    // USE CASE 테스트
+    @Test("🟢 - 피드 리스트 가져오기")
+    func fetch_nearby_feeds() async throws {
+        // Feeds
+        // FetchNearbyFeeds
+
+        #expect(false)
     }
-
 }
+
+// MARK: - 1. 도메인 식별 / 구현
+
+import CoreLocation
+import Foundation
+import MemberwiseInit
+
+@MemberwiseInit(.public)
+struct Feed: Equatable, Identifiable {
+    let id: UUID = .init()
+    @Init(.public) let caption: String
+}
+
+/// USE TYPED-THROWS
+protocol FetchNearbyFeedsUseCase {
+    func execute(around location: CLLocation?) async throws
+}
+
+
--- CoCoTests/SUT.swift
+++ CoCoTests/SUT.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+import MemberwiseInit
+
+@MemberwiseInit(.public)
+struct Message: Equatable, Identifiable {
+    let id: UUID = .init() // private(set)과 같은건지
+    @Init(.public) let content: String
+    let sentAt: Date = .init()
+
+    var isValid: Bool {
+        !content.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
+    }
+}
